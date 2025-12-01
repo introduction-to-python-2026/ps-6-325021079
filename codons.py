f60f25544c34cc068d1a4ab6ca9beb13cfcb2e1c
325021079 <shenhavshkalim@gmail.com>
2025-12-01 17:36:47 +0200

    Update codons.py

--- codons.py
+++ codons.py
@@ -1,10 +1,5 @@
-
-
 def create_codon_dict(file_path):
-    my_dic = {} 
-    for row in rows:
-        row = row.strip().split('\t')
-        codon = row[0]
-        amino_acid = row[2]
-        my_dic[codon] = amino_acid
+    my_dic = {}
+    with open(file_path) as codons_file:
+        my_dic = {row.strip().split('\t')[0]: row.strip().split('\t')[2] for row in codons_file.readlines()}
     return my_dic
